0d44ac0a94fa0c84862075edd05a6826af52c4a7
GUSTAVO FERNANDO DE FRAZÃO LIMA <frazao39@gmail.com>
2023-03-03 17:10:52 -0300

    fixed

--- src/controller/UrlsController.js
+++ src/controller/UrlsController.js
@@ -72,12 +72,12 @@ export async function deleteUrl (req, res) {
       const urlToDelete  = await db.query('DELETE FROM url WHERE id = $1', [id])
       const selecUrl = await db.query(`SELECT * FROM url WHERE id=$1;`, [id]);
       console.log(selecUrl)
-      if (selecUrl.rows == 0) return res.sendStatus(204)
+      // if (selecUrl.rows == 0) return res.sendStatus(404)
       if(userId !== selecUrl.rows[0].userId) {
           return res.sendStatus(401)
 
       }else{
-        return res.status(404).send(urlToDelete )
+        return res.status(204).send(urlToDelete )
       }    
     } catch (error) {
       res.status(500).send(error.message);
